e77454ba896b6955bfec40b9338e9b799a4e7e89
Dean Uziel <92694814+Dean99712@users.noreply.github.com>
2023-09-10 16:20:50 +0300

    first form

---
+++ config.py
@@ -0,0 +1,7 @@
+class Config(object):
+    Driver = "{ODBC Driver 18 for SQL Server}"
+    Server = "tcp:flask-server-database.database.windows.net"
+    Port = 1433
+    Database = "bad_at_soccer"
+    Uid = "dbadmin"
+    Pwd = "Admin123"
