4898e44d89feaaad90a0a6b79bef5cc4cf4a578b
Dean Uziel <92694814+Dean99712@users.noreply.github.com>
2024-03-24 18:43:21 +0200

    update logger.py

--- logger.py
+++ logger.py
@@ -4,7 +4,7 @@ import logging
 date = datetime.datetime.now().strftime('%Y-%m-%d')
 location = f'{date}-log.log'
 
-logging.basicConfig(filename=location, level=logging.DEBUG,
+logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 
 logger = logging.getLogger(__name__)
