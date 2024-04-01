d7d95401d7404e524d17f7ff7bdcf94e42544bc5
Dean Uziel <92694814+Dean99712@users.noreply.github.com>
2024-04-01 20:30:11 +0300

    auth load credentials update

--- config.py
+++ config.py
@@ -1,14 +1,23 @@
-import os
-
-from dotenv import load_dotenv
-
-load_dotenv()
+# import os
+#
+# from dotenv import load_dotenv
+#
+# load_dotenv()
+#
+#
+# class Config:
+#     driver = os.getenv('DRIVER')
+#     server = os.getenv('SERVER')
+#     port = os.getenv('PORT')
+#     database = os.getenv('DATABASE')
+#     uid = os.getenv('UID')
+#     password = os.getenv('PASSWORD')
 
 
 class Config:
-    driver = os.getenv('DRIVER')
-    server = os.getenv('SERVER')
-    port = os.getenv('PORT')
-    database = os.getenv('DATABASE')
-    uid = os.getenv('UID')
-    password = os.getenv('PASSWORD')
+    driver = "{ODBC Driver 18 for SQL Server}"
+    server = "tcp:flask-server-database.database.windows.net"
+    port = 1433
+    database = "bad_at_soccer"
+    uid = "dbadmin"
+    password = "Admin123"
